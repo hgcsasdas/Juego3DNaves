c568253ce636d25bf9dd309fed2fabce020861d0
hgc88 <csanchezn3@alumnos.nebrija.es>
2023-01-19 23:15:01 +0100

    Avión funcional

---
+++ Naves/src/Scripts/AvionMovimiento.gd
@@ -0,0 +1,81 @@
+extends Camera
+
+
+#########################
+# EXPORT PARAMS
+#########################
+# speed
+var velocidad = 10
+var turbo = 0
+# guiñada ~ guiniada (yaw) girar en eje y
+var guiniada_vel = 10
+var max_guiniada = 70
+# girar (roll) poner el avión de lado
+var max_giro = 70.0
+var giro = 1
+var giro_vel = 10
+# inclinar (pitch) inclinación o declinación de los aviones
+var max_inclinacion = 30
+
+var tiempo_turbo = 5
+
+var gravedad = -9.81
+
+onready var model = $Plane
+
+func _process(delta: float) -> void:
+	_move(delta, turbo)
+	arriba(delta)
+	abajo(delta)
+	girarIzq(delta)
+	girarDer(delta)
+	rotarDer(delta)
+	rotarIzq(delta)
+	turbo()
+
+#	_pitch(delta)
+#	_yaw(delta)
+
+
+#########################
+# MOVEMENT FUNCTIONS
+#########################
+func _move(delta: float, turbo: float) -> void:
+	translation -= transform.basis.z * delta * velocidad * turbo
+
+func arriba(delta: float) -> void:
+	if Input.is_action_pressed("arriba"):
+		rotation_degrees.x += delta * giro_vel + 0.75
+
+func abajo(delta: float) -> void:
+	if Input.is_action_pressed("abajo"):
+		rotation_degrees.x -= delta * giro_vel + 0.75
+		
+func girarIzq(delta: float) -> void:
+	if Input.is_action_pressed("girarIzq"):
+		rotation_degrees.y += delta * giro_vel + 0.75
+
+func girarDer(delta: float) -> void:
+	if Input.is_action_pressed("girarDer"):
+		rotation_degrees.y -= delta * giro_vel + 0.75
+		
+func rotarIzq(delta: float) -> void:
+	if Input.is_action_pressed("virarIzq"):
+		#model.set_rotation(Vector3(-90, -90,90))
+		if rotation_degrees.z < max_giro:
+			giro += 1 * delta
+			print(giro)
+			model.rotate(Vector3(0, 0, 1), delta)
+		
+func rotarDer(delta: float) -> void:
+	if Input.is_action_pressed("virarDer"):
+		model.rotate(Vector3(0, 0, -1),delta)
+
+func turbo() -> void:
+	if Input.is_action_pressed("turbo"):
+		turbo = 5.0
+	else:
+		turbo = 1
+
+func pepe():
+	return 	
