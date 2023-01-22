6626df37b3e302ffd41c9755d385ce01a9424536
hgc88 <csanchezn3@alumnos.nebrija.es>
2023-01-22 18:56:17 +0100

    rotacion enemigo

--- Naves/src/Scripts/antiaEreo.gd
+++ Naves/src/Scripts/antiaEreo.gd
@@ -2,16 +2,18 @@ extends Area
 
 onready var antiaereo = $Antiaircraft_gun
 onready var raycast = $RayCast
-onready var plane = get_node("../Avion/Kplayer/Plane_modelo")
+onready var plane = get_node("../Avion/Kplayer/Position3D")
+onready var laserScene = preload("res://src/Escenas/laserEnemigo.tscn")
+onready var pivote = get_node("Antiaircraft_gun/Position3D")
 
 var onRadius = false
 
 func _process(delta):
 
 	var playerPos = plane.global_transform.origin
-	print(playerPos)
+	#print(playerPos)
 	
-	#disparar()
+	disparar()
 	
 
 	antiaereo.look_at(Vector3(playerPos.x, global_transform.origin.y, playerPos.z), Vector3.UP)
@@ -23,12 +25,16 @@ func _process(delta):
 	
 func apuntarJugador(delta: float) -> void:
 		antiaereo.rotation_degrees.y += delta * 100
-"""""	
+
 func disparar():
 	if onRadius:
-		print("pitopito")
-	else: 
-		print("")"""
+		var laser = laserScene.instance()
+		var sceneLaser = get_tree().root.get_children()[0]
+		sceneLaser.call_deferred("add_child", laser)
+		laser.parentName = self.name
+		laser.global_transform = pivote.global_transform
+		laser.global_transform = pivote.global_transform
+
 		
 func _on_Area_body_entered(body):
 	onRadius = true
