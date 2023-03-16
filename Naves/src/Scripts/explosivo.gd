4ffe46bdff6ede761d8baa36b91b7559bec82653
hgcsasdas <csanchezn3@alumnos.nebrija.es>
2023-03-16 13:46:07 +0100

    v1.1 alpha

--- Naves/src/Scripts/explosivo.gd
+++ Naves/src/Scripts/explosivo.gd
@@ -1,34 +1,35 @@
 extends Spatial
 
-# Declare member variables here. Examples:
 onready var todo = get_node("../../objetivo")
-
-#VARIABLE DE VIDA
+onready var explosionAnimation = preload("res://src/Escenas/explosion.tscn")
+onready var pivot = get_node("../../explosionContainer")
+var timer
 var vida = 1
 var damage = 50
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass
 
 func reci(damage):
 	vida -= damage
 	if vida <= 0:
-		explotar()
+		todo.queue_free()
+		var explosion = explosionAnimation.instance() 
+		get_tree().get_root().call_deferred("add_child", explosion)
+		explosion.global_transform = pivot.global_transform
+		timer = Timer.new()
+		timer.set_wait_time(3)
+		timer.set_one_shot(true)
+		timer.connect("timeout", self, "cambiar_escena")
+		add_child(timer)
+		timer.start()
+
+func cambiar_escena():
+	get_tree().change_scene("res://src/Menu/Menu.tscn")
+
 
 func _on_Area_area_entered(area):
 	reci(damage)
-	pass # Replace with function body.
-
 
 func _on_Area_body_entered(body):
-	pass # Replace with function body.
-
-func explotar():
-	todo.queue_free()
+	pass
